bdac00fc4fd085eb5655d6f33d48e231e33d4a1f
YVDhasith <yvkalanadhasith99@gmail.com>
2024-06-23 00:55:29 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,18 @@ public class Main {
         System.out.println("Student age: " + studentAge);
         System.out.println("Student fee: " + studentFee);
         System.out.println("Student grade: " + studentGrade);
+
+        // Create integer variables
+        int length = 4;
+        int width = 6;
+        int area;
+
+// Calculate the area of a rectangle
+        area = length * width;
+
+// Print variables
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of the rectangle is: " + area);
     }
 }
